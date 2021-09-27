e0d9ae20e1e9458cd7c4577eaa7d9b129614f42c
Boris <47457848+boris-chb@users.noreply.github.com>
2021-09-27 20:33:04 +0100

    Added infinite scroll functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Infinte Scroll
 
 Project built with HTML, CSS and JavaScript fetching data via [Unsplash API](https://unsplash.com/) providing infinite scroll social-media like functionality.
+
+![Infinite Scroll Logic](./scroll.png)
--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
+
+let pageReady = false; // Page load status
+let imagesLoaded = 0;
+let totalImagesLoaded = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30; // How many images you want to load in one API call
 const apiKey = "QNRl5YO8zWJyhtGw96YRg5GBZXVaKHg3U-727CxqDVM";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+const imageLoaded = () => {
+  imagesLoaded++;
+  if (imagesLoaded === totalImagesLoaded) {
+    pageReady = true;
+    loader.hidden = true;
+  }
+};
 
 const setAttributes = (element, attributes) => {
   // Helped function to set multiple attributes to an HTML element
@@ -14,7 +26,10 @@ const setAttributes = (element, attributes) => {
   }
 };
 
+// Create Elements for Links & Photos to add to DOM
 const displayPhotos = () => {
+  imagesLoaded = 0;
+  totalImagesLoaded = photosArray.length;
   photosArray.forEach((photo) => {
     // Create <a> that links to Unsplash
     const item = document.createElement("a");
@@ -22,16 +37,18 @@ const displayPhotos = () => {
 
     setAttributes(item, {
       href: photo.links.html,
-      target: "_blank"
+      target: "_blank",
     });
+
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
-      title: photo.alt_description
+      title: photo.alt_description,
     });
+
+    img.addEventListener("load", imageLoaded);
     item.appendChild(img);
     imageContainer.appendChild(item);
-    console.log(imageContainer);
   });
 };
 
@@ -46,8 +63,17 @@ const getPhotos = async () => {
   }
 };
 
+// If scrolled near (-1000px) bottom of the page, invoke getPhotos()
 window.addEventListener("scroll", () => {
-  console.log("scrolled");
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    pageReady
+  ) {
+    // Reset page status to not ready such that scroll eventListener will be triggered again
+    pageReady = false;
+    getPhotos();
+  }
 });
 
+// On Load
 getPhotos();
